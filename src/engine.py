c1651f74d02104854dbddf9237b1a6263eb4e0b6
simplycode07 <simplycode07@gmail.com>
2025-01-15 22:34:39 +0530

    moved Game object to engine.py

---
+++ src/engine.py
@@ -0,0 +1,37 @@
+import pygame
+
+from . import level, settings
+
+from .scene import Renderer
+from .physics_entities import PhysicsEntities
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.display = pygame.display.set_mode(settings.screen_res)
+        self.clock = pygame.time.Clock()
+        
+        self.level_manager = level.LevelManager("saves/test.save")
+
+        level_info = self.level_manager.load_tilemap(0)
+        self.physics_module = PhysicsEntities(level_info)
+        # self.player = Sprite(level_info[0], level_info[1])
+        # self.hoop = Hoop(level_info[2])
+        self.renderer = Renderer(settings.screen_res)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                self.physics_module.input(event)
+                # self.player.handle_input(event)
+
+            # self.player.update(1/settings.physics_fps, self.display)
+            self.physics_module.update(1/settings.physics_fps, self.display)
+            self.renderer.render(self.display, self.physics_module.player, self.physics_module.hoop, self.physics_module.tilemap)
+            pygame.display.update()
+            self.clock.tick(settings.update_fps)
