b2fccf7d69129a41d19c716d50a586340e76a78f
simplycode07 <simplycode07@gmail.com>
2025-01-18 00:54:33 +0530

    fixed typo and changed a function name

--- src/physics_entities/__init__.py
+++ src/physics_entities/__init__.py
@@ -13,7 +13,7 @@ class PhysicsEntities:
         self.player = Sprite(self.tilemap, player_info)
         self.hoop = Hoop(hoop_info)
 
-    def input(self, event):
+    def handle_input(self, event):
         self.player.handle_input(event)
 
     def update(self, delta, display):
@@ -34,7 +34,7 @@ class PhysicsEntities:
     # this checks if any passed object is near the player to do physics
     # the object should have the following attributes
     # 1) x, y -> current position
-    # 2)collision range -> basically the area in which you wanna check for collision
+    # 2) collision range -> basically the area in which you wanna check for collision
     def object_near_player(self, entity) -> bool:
         if abs(self.player.pos[0] - entity.pos[0]) < entity.collision_range:
             if abs(self.player.pos[1] - entity.pos[1]) < entity.collision_range:
