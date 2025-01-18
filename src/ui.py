1e18b025dcbc40d4548f46d8a98e78b6ed4aba49
simplycode07 <simplycode07@gmail.com>
2025-01-19 00:18:52 +0530

    Added positioning to text rendering

--- src/ui.py
+++ src/ui.py
@@ -14,14 +14,19 @@ class UIState(Enum):
 
 class UIManager:
     def __init__(self):
-        self.comic_sans = pygame.font.SysFont('Comic Sans MS', 30)
+        self.comic_sans = pygame.font.SysFont('Comic Sans MS', 60)
 
     def draw(self, surface:pygame.Surface, curr_state:"UIState"):
         if curr_state == UIState.MENU:
-            self.draw_text(surface, "Press Enter to Start")
+            self.draw_text(surface, "Press Enter to Start", [settings.screen_width//2, settings.screen_height//2])
         pass
 
 
-    def draw_text(self, surface, text):
+    def draw_text(self, surface, text, pos=[0, 0], align_center=True):
         text_surface = self.comic_sans.render(text, False, (255, 255, 255))
-        surface.blit(text_surface, (0, 0))
+
+        if align_center:
+            pos[0] -= text_surface.get_width()//2
+            pos[1] -= text_surface.get_height()//2
+
+        surface.blit(text_surface, pos)
