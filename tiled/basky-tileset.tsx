3851675fb246ad68018bb85098ae98df01d47da6
simplycode07 <simplycode07@gmail.com>
2025-02-07 17:11:18 +0530

    added direction wise spikes

--- tiled/basky-tileset.tsx
+++ tiled/basky-tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.11.1" name="basky" tilewidth="32" tileheight="32" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -24,5 +24,32 @@
    <property name="name" value="-2"/>
   </properties>
   <image source="../assets/basky_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="2"/>
+  </properties>
+  <image source="../assets/spike_east.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="3"/>
+  </properties>
+  <image source="../assets/spike_west.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="4"/>
+  </properties>
+  <image source="../assets/spike_north.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.26001" y="0.0840005" width="33.7682" height="14.9521"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="5"/>
+  </properties>
+  <image source="../assets/spike_south.png" width="32" height="32"/>
  </tile>
 </tileset>
